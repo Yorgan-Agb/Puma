3a046e6a307886b69d54c0ee5865d818ead479a5
agblemagnon.yorgan@gmail.com <agblemagnon.yorgan@gmail.com>
2022-09-02 18:13:37 +0200

    first co

---
+++ index.js
@@ -0,0 +1,20 @@
+let upMoveInfo = document.querySelector('textCacher')
+
+document
+  .getElementById('fleche')
+  .animate(
+    [{ transform: 'translateY(1%)' }, { transform: 'translateY(10%)' }],
+    {
+      duration: 500,
+      iterations: Infinity,
+    }
+  )
+
+const moveUp = () => {
+  let changeText = document.getElementById('bottomTitle')
+  changeText.innerHTML = "J'Y VAIS"
+  let moveVideo = document.getElementById('videoPuma')
+  moveVideo.style.display = 'none'
+  let upDeoContainer = document.getElementById('textHide')
+  upDeoContainer.style.transform = 'translateY(-50%)'
+}
